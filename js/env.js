d5f53deb6894145acf9af769b97d7c9387ebbbc1
John Chernoff <jachernoff@gmail.com>
2023-06-03 22:26:31 -0700

    Tooltips in move list

--- js/env.js
+++
@@ -1,6 +0,0 @@
-class MOLE_ENV {
-    static port = "5555";
-    static local_host = "ws://localhost:" + this.port;
-    static remote_host = "wss://molechess.com/server";
-    static host = this.local_host;
-}
